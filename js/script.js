2ced7195cf408f54de514152ce6ba20e7a527350
MarikoAsai <faint124@me.com>
2020-10-21 16:57:42 +0900

    initial commit

---
+++ js/script.js
@@ -0,0 +1,71 @@
+$(function () {
+    // drawer.js
+    $('.drawer').drawer();
+
+    // smoothscroll
+    // #から始まるURLがクリックされた時
+    $('a[href^="#"]').click(function () {
+        // .headerクラスがついた要素の高さを取得
+        let header = jQuery(".header").innerHeight();
+        let speed = 300;
+        let id = jQuery(this).attr("href");
+        let target = jQuery("#" == id ? "html" : id);
+        // トップからの距離からヘッダー分の高さを引く
+        let position = jQuery(target).offset().top - header;
+        // その分だけ移動すればヘッダーと被りません
+        jQuery("html, body").animate(
+            {
+                scrollTop: position
+            },
+            speed
+        );
+        return false;
+    });
+
+    // wow.js
+    new WOW().init()
+
+    // google-form
+    let $form = $('#js-form')
+    $form.submit(function (e) {
+        $.ajax({
+            url: $form.attr('action'),
+            data: $form.serialize(),
+            type: "POST",
+            dataType: "xml",
+            statusCode: {
+                0: function () {
+                    //送信に成功したときの処理 
+                    $form.slideUp()
+                    $('#js-success').slideDown()
+                },
+                200: function () {
+                    //送信に失敗したときの処理 
+                    $form.slideUp()
+                    $('#js-error').slideDown()
+                }
+            }
+        });
+        return false;
+    });
+
+
+    //   フォームの入力確認
+    let $submit = $( '#js-submit' )
+    $('#js-form input, #js-form textarea').on('change', function () {
+        if (
+            $('#js-form input[type="text"]').val() !== "" &&
+            $('#js-form input[type="email"]').val() !== "" &&
+            $('#js-form input[name="entry.1077114607"]').prop('checked') === true
+
+        ) {
+            // 全て入力された時
+            $submit.prop('disabled', false)
+            $submit.addClass('-active')
+        } else {
+            // 全て入力されていない時
+            $submit.prop('disabled', true)
+            $submit.removeClass('-active')
+        }
+    })
+})
